37a3232ee9b0658ff7e74330eb12a1b467478b24
shyanfeng <shyanfeng@gmail.com>
2014-12-04 16:20:55 +0800

    repush

--- src/Square.c
+++ src/Square.c
@@ -138,10 +138,8 @@ void eliminateNumberFromPeers(int squares[9][9], Square *peers, int row, int col
         int *value2 = getSquare(squares, ((peers[i].row) + 1), ((peers[i].column) + 1));
         squareDelNumber(value2, decimalValue);
         int value1 = *value2;
-        printf("%d\n", value1);
         if(value1 == 0){
           Throw(ERR_EMPTY_SQU);
-          printf("halo\n");
         }else if(squareContainOneNumbers(squares, ((peers[i].row) + 1), ((peers[i].column) + 1)) == 1){
           int value2 = checkBinaryValue(value1);
           eliminateNumberFromAllPeers(squares, ((peers[i].row) + 1), ((peers[i].column) + 1), value2);
