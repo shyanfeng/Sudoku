6329fabfda937f27e94867d7f7341e6cc8d933f3
Shyanfeng <Shyanfeng@gmail.com>
2015-01-18 17:23:26 +0800

    typecast solved

--- test/test_EliminateNumberFromAllPeers.c
+++ test/test_EliminateNumberFromAllPeers.c
@@ -19,7 +19,7 @@ void test_EliminateNumberFrommAllPeers_for_mock_sudoku1_1(void){
   searchPossibilityValueOfEmptySquare(square);
   dumpSquare(square);
   
-  _eliminateNumberFromAllPeers_Expect(square, 9, 2, 5);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 9, 2, 5);
   
   eliminateNumberFromAllPeers(square, 1, 2, 5);
 }
@@ -32,8 +32,8 @@ void test_EliminateNumberFrommAllPeers_for_mock_sudoku1_2(void){
   searchPossibilityValueOfEmptySquare(square);
   dumpSquare(square);
   
-  _eliminateNumberFromAllPeers_Expect(square, 3, 3, 5);
-  _eliminateNumberFromAllPeers_Expect(square, 1, 6, 9);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 3, 3, 5);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 1, 6, 9);
   
   eliminateNumberFromAllPeers(square, 1, 3, 5);
 }
@@ -58,7 +58,7 @@ void test_EliminateNumberFrommAllPeers_for_mock_sudoku2_2(void){
   searchPossibilityValueOfEmptySquare(square);
   dumpSquare(square);
   
-  _eliminateNumberFromAllPeers_Expect(square, 4, 3, 2);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 4, 3, 2);
   
   eliminateNumberFromAllPeers(square, 4, 1, 2);
   
@@ -72,7 +72,7 @@ void test_EliminateNumberFrommAllPeers_for_mock_sudoku2_3(void){
   searchPossibilityValueOfEmptySquare(square);
   dumpSquare(square);
   
-  _eliminateNumberFromAllPeers_Expect(square, 4, 9, 1);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 4, 9, 1);
   
   eliminateNumberFromAllPeers(square, 4, 5, 3);
   
@@ -98,8 +98,8 @@ void test_EliminateNumberFrommAllPeers_for_mock_sudoku2_5(void){
   searchPossibilityValueOfEmptySquare(square);
   dumpSquare(square);
   
-  _eliminateNumberFromAllPeers_Expect(square, 6, 2, 5);
-  _eliminateNumberFromAllPeers_Expect(square, 5, 5, 9);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 6, 2, 5);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 5, 5, 9);
   
   eliminateNumberFromAllPeers(square, 5, 2, 5);
   
@@ -113,22 +113,8 @@ void test_EliminateNumberFrommAllPeers_for_mock_sudoku2_6(void){
   searchPossibilityValueOfEmptySquare(square);
   dumpSquare(square);
   
-  _eliminateNumberFromAllPeers_Expect(square, 8, 4, 4);
+  _eliminateNumberFromAllPeers_Expect((ARRAY_OF_9_9)square, 8, 4, 4);
   
   eliminateNumberFromAllPeers(square, 1, 4, 4);
   
 }
-/*
-void test_EliminateNumberFrommAllPeers_for_mock_sudoku2_7(void){
-  char read[81] = "61.9..8.3.4.7......8.......89256437137429....156.......2..7..5..3.48.6......1....";
-  int square[9][9];
-  clearSquares(square);
-  grids(read, square);  
-  searchPossibilityValueOfEmptySquare(square);
-  dumpSquare(square);
-  
-  _eliminateNumberFromAllPeers_Expect(square, 8, 4, 4);
-  
-  eliminateNumberFromAllPeers(square, 1, 4, 4);
-  
-}*/
